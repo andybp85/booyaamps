4d7ee2622fcfbbb7bc69bf64c2954be11c9a4473
Andrew Stanish <andy@youshallthrive.com>
2015-10-11 22:47:39 -0400

    watch task working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,28 @@
 'use strict';
 
+var LIVERELOAD_PORT = 35729,
+    lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT}),
+    gateway = require('gateway'),
+    serveStatic = require('serve-static'),
+    path = require('path'),
+    mountFolder = function (connect, dir) {
+        return serveStatic(require('path').resolve(dir));
+    };
+
 module.exports = function(grunt) {
     require('time-grunt')(grunt);
     require('load-grunt-tasks')(grunt);
 
-    // Configurable paths
     var config = {
         app  : require('./bower.json').appPath || 'src',
         dist : 'dist',
         tmp  : '.tmp'
     };
 
-
     grunt.initConfig({
+
+        yeoman : config,
+
         pkg: grunt.file.readJSON('./package.json'),
         composer: grunt.file.readJSON('./composer.json'),
 
@@ -65,7 +75,122 @@ module.exports = function(grunt) {
             application: {
                 configuration: './phpunit.xml.dist'
             }
+        },
+
+
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= yeoman.dist %>/*'
+                    ]
+                }]
+            },
+            server: '.tmp'
+        },
+
+        watch: {
+            javascript: {
+                files: ['<%= yeoman.app %>/scripts/**/*.js'],
+                tasks: ['jshint']
+            },
+            sass: {
+                files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+                tasks: ['sass']
+            },
+            bower: {
+                files: ['<%= yeoman.app %>/bower_components/**/*.js'],
+                tasks: ['bower']
+            },
+            livereload: {
+                options: {
+                    livereload: LIVERELOAD_PORT
+                },
+                files: [
+                    '<%= yeoman.app %>/{,*/}*.html',
+                    '<%= yeoman.app %>/{,*/}*.php',
+                    '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
+                    '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
+                    '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+
+        sass: {
+            all: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    '.tmp/styles/main.css': '<%= yeoman.app %>/styles/main.scss'
+                }
+            }
+        },
+
+        concurrent: {
+            server: [
+                'sass'
+            ],
+            test: [
+                'sass'
+            ],
+            dist: [
+                'sass',
+                'imagemin',
+                'svgmin'
+            ]
+        },
+
+        open: {
+            app: {
+                path: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/index.php'
+            },
+            dist: {
+                path: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/index.php'
+            },
+            report: {
+                path: 'docs/complexity/index.html'
+            }
+        },  
+
+        connect: {
+            options: {
+                port: 9000,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: '127.0.0.1'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            gateway(__dirname + path.sep + config.app, {
+                                '.php': 'php-cgi'
+                            }),
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, config.app)
+                        ];
+                    }
+                }
+            }
         }
+    });
+
+
+    grunt.registerTask('serve', function(target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open:dist', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean:server',
+            'concurrent:server',
+            'connect:livereload',
+            'open:app',
+            'watch'
+        ]);
     });
 
     grunt.task.registerTask('build', 'Building', function() {
