b37a67ca9d698017845be30c9d76e6a2ce627314
Andrew Stanish <andy@youshallthrive.com>
2015-10-25 22:55:39 -0400

    ajax route and Doctrine working

--- app/src/app.php
+++ app/src/app.php
@@ -3,7 +3,7 @@
 // Smarty setup
 // -----------------------------------------------------------------------------
 
-// Setup our renderer and add some global variables
+// Setup our renderer 
 $app->view(new \Slim\Views\Smarty());
 
 $view = $app->view();
@@ -24,12 +24,13 @@ $smarty = $view->getInstance();
 $smarty->setCompileId((bool) IS_AJAX ? 'ajaxResponse' : $baseTemplate);
 
 
+
 // Pages Routes
 // -----------------------------------------------------------------------------
 
 $app->get('/', function() use($app, $smarty) {
-   $app->render('pages/home.tpl', array('ParentTemplate' => $smarty->compile_id . '.tpl'));
-   //$app->render('admin/editor.tpl', array('ParentTemplate' => $smarty->compile_id . '.tpl'));
+   //$app->render('pages/home.tpl', array('ParentTemplate' => $smarty->compile_id . '.tpl'));
+   $app->render('admin/amps.tpl', array('ParentTemplate' => $smarty->compile_id . '.tpl'));
 })->name('home');
 
 $app->get('/about', function() use($app, $smarty) {
@@ -75,6 +76,44 @@ $app->get('/admin/amps', function() use($app, $smarty) {
 $app->get('/admin/mods', function() use($app, $smarty) {
    $app->render('admin/editor.tpl', array('ParentTemplate' => $smarty->compile_id . '.tpl'));
 })->name('admin');
+
+
+// AJAX Routes
+// -----------------------------------------------------------------------------
+
+$app->get('/ajax', function() {
+    try {
+        $db = getConnection();
+        $db = null;
+        echo 'success';
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+    
+})->name('ajax');
+
+// Functions
+// -----------------------------------------------------------------------------
+
+function getConnection() {
+    require 'vendor/autoload.php';
+    
+    $dotenv = new Dotenv\Dotenv(__dir__);
+    $dotenv->load();
+
+    $connectionParams = array(
+        'dbname'   => $_ENV['BOOYA_DB'],
+        'user'     => $_ENV['BOOYA_DB_USER'],
+        'password' => $_ENV['BOOYA_DB_PASSWD'],
+        'host'     => $_ENV['BOOYA_DB_HOST'],
+        'driver'   => 'pdo_mysql'
+    ); 
+
+    $config = new \Doctrine\DBAL\Configuration();
+
+    return \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+
+}
 
 
 
